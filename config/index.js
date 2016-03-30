06083fda90365de6f1884bf737c3fe5249cbe1bb
Jordan Cotter <jordan.cotter@ninecollective.com>
2016-03-30 08:42:39 -0500

    removed sensative data

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 exports = module.exports = {
 
     "aws_credentials": {
-        "accessKeyId": "AKIAIFOVBC4EEUNX3RTQ",
-        "secretAccessKey": "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",
+        "accessKeyId": "",
+        "secretAccessKey": "",
         "region": "us-west-2"
     },
 
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: 'Chic@g0.1908',
+        password: '',
         database: 'videoUploader',
         multipleStatements: true,
         connectionLimit: 300
